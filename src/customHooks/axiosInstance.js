15d2a904b1899ef31b7c848c869bcffaed10995e
AbinDreamCare <abin.thomas@dreamcaredevelopers.com>
2023-07-25 15:02:13 +0530

    Update axiosInstance.js

--- src/customHooks/axiosInstance.js
+++ src/customHooks/axiosInstance.js
@@ -1,11 +1,11 @@
 import  axios  from 'axios';
 
-// const instance = axios.create({
-//     baseURL: process.env.base_url_axios
-// })
-
 const instance = axios.create({
-    baseURL: 'http://localhost:7000/'
+    baseURL: process.env.base_url_axios
 })
+
+// const instance = axios.create({
+//     baseURL: 'http://localhost:7000/'
+// })
 
 export default instance
